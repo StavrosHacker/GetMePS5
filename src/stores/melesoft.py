340ab1e77960327fd35b184356a2144afead2ea9
Stavros Nicolaou <stavros_nicolaou@cytanet.com.cy>
2022-02-19 11:29:26 +0200

    Fix Melesoft (mild changes)

--- src/stores/melesoft.py
+++ src/stores/melesoft.py
@@ -17,8 +17,8 @@ def melesoftSearch(ps5):
     try:
         response = requests.get(ps5_link).text
         soup = BeautifulSoup(response, "html.parser")
-        status = soup.find("meta", itemprop="availability")
-        if status["content"] != "http://schema.org/OutOfStock":
+        status = soup.body.findAll(text=re.compile("Δεν υπάρχει στο απόθεμα."))
+        if not status:
             msg = logger("PS5 " + ps5_name + " is in STOCK on " + site_domain + "\n\nBuy now -> " + ps5_link, 1)
             alerts(msg, 1)
         else:
