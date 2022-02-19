a1992d2ca0bd6e6329da2383540224a3a8c5d9fe
Stavros Nicolaou <stavros_nicolaou@cytanet.com.cy>
2022-02-19 11:24:28 +0200

    Fix FidelityCenter (mild changes)

--- src/stores/fidelitycenter.py
+++ src/stores/fidelitycenter.py
@@ -10,14 +10,18 @@ alerts = alerts.alerts
 def fidelityCenterSearch(ps5):
     site_domain = "fidelitycenter.com.cy"
     ps5_name = "Disc Edition"
-    ps5_link = "https://www.fidelitycenter.com.cy/product/sony-playstation5/"
+    ps5_link = "https://www.fidelitycenter.com.cy/product/sony-playstation-5-ps5/"
+    
+    if ps5 == "digital":
+        ps5_name = "Digital Edition"
+        ps5_link = "https://www.fidelitycenter.com.cy/product/sony-playstation-5-ps5-digital-edition/"
     
     logger("Searching on " + site_domain + "..", 0)
 
     try:
         response = requests.get(ps5_link).text
         soup = BeautifulSoup(response, "html.parser")
-        status = soup.body.findAll(text=re.compile("This product is currently out of stock and unavailable."))
+        status = soup.body.findAll(text=re.compile("OUT OF STOCK"))
         if not status:
             msg = logger("PS5 " + ps5_name + " is in STOCK on " + site_domain + "\n\nBuy now -> " + ps5_link, 1)
             alerts(msg, 1)
