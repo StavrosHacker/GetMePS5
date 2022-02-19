aec3c5c01beadbee4d7f3d3cb439b6f659ef37e4
Stavros Nicolaou <stavros_nicolaou@cytanet.com.cy>
2022-02-19 11:11:25 +0200

    Fix Public Cyprus (new backend)

--- src/stores/public.py
+++ src/stores/public.py
@@ -6,22 +6,22 @@ logger = logger.logger
 alerts = alerts.alerts
 
 def publicSearch(ps5):
-    site_domain = "public-cyprus.com.cy"
+    site_domain = "public.cy"
     ps5_name = "Disc Edition"
-    ps5_buy = "https://www.public-cyprus.com.cy/product/"
-    ps5_link = "https://www.public-cyprus.com.cy/cy/common/updateProductInfo.jsp?product="
-    ps5_id = "prod10238545pp"
+    ps5_buy = "https://www.public.cy/product/gaming/consoles/ps5/"
+    ps5_link = "https://www.public.cy/public/v2/sku/"
+    ps5_id = "1428499"
 
     if ps5 == "digital":
         ps5_name = "Digital Edition"
-        ps5_id = "prod10810253pp"
+        ps5_id = "1530295"
 
     logger("Searching on " + site_domain + "..", 0)
     
     try:
         response = requests.get(ps5_link + ps5_id).json()
-        status = response['productInfo'][0]['stockRule']
-        if status != "εξαντλήθηκε!":
+        status = response['sku']['miraklHasStock']
+        if status != "false":
             msg = logger("PS5 " + ps5_name + " is in STOCK on " + site_domain + "\n\nBuy now -> " + ps5_buy + ps5_id + "/", 1)
             alerts(msg, 1)
         else:
